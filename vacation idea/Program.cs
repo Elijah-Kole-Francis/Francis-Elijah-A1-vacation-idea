c5ade33c559a0a845fc008b7398cabb7d7ec79b6
elija <elija@LOUBITAN>
2024-09-16 21:07:48 -0400

    This should be the final product, can't figure out the bool

--- vacation idea/Program.cs
+++ vacation idea/Program.cs
@@ -10,8 +10,8 @@ string inputLocation = Console.ReadLine();
 
 if (inputLocation == beach)
 {
-    Console.WriteLine($"You decided to spend the afternoon at the beach. What will you do? \nSwim? \nGet a tan? \nSurf?");
-    string input2 = Console.ReadLine();
+    Console.WriteLine($"You decided to spend the afternoon at the {beach}. What will you do? \nSwim? \nGet a tan? \nSurf?");
+    string inputBeach = Console.ReadLine();
 
     //Gotta remember to isolate the input strings because they're individual variables, otherwise code breaks.
     // e.g. input, input2, input3, etc. Assign proper names/values later
@@ -20,19 +20,19 @@ if (inputLocation == beach)
     string tan = "tan";
     string surf = "surf";
 
-    if (input2 == swim)
+    if (inputBeach == swim)
     {
-        Console.WriteLine($"You swam all afternoon until dusk, impressive! But, now you're tired. It's time to go home and rest.");
+        Console.WriteLine($"You {swim} all afternoon until dusk, impressive! But, now you're tired. It's time to go home and rest.");
     }
 
-    else if (input2 == tan)
+    else if (inputBeach == tan)
     {
-        Console.WriteLine($"You decided to lay down in the sun ans get a tan. Uh oh, you fell asleep! Better go get some cream for those burns...");
+        Console.WriteLine($"You decided to lay down in the sun ans get a {tan}. Uh oh, you fell asleep! Better go get some cream for those burns...");
     }
 
-    else if (input2 == surf)
+    else if (inputBeach == surf)
     {
-        Console.WriteLine($"Time to hang ten and shred that gnar! Be sure not to wipeout!");
+        Console.WriteLine($"Time to hang ten and shred that gnar! Be sure not to wipeout while you {surf}!");
     }
 }
 //Best to do the rule of 3s, three initial choices leading into 3 more choices
@@ -41,58 +41,58 @@ if (inputLocation == beach)
 //isolate the mountains and town 'else if' statements, they're failing because they're nested within the 'beach' if statement
 else if (inputLocation == mountains)
 {
-    Console.WriteLine($"You hike up a tall mountain to soak in a wonderful view. While you're here you may as well do something. \nFish? \nCatch bugs? \nHunt?");
-    string input3 = Console.ReadLine();
+    Console.WriteLine($"You hike up a tall {mountains} to soak in a wonderful view. While you're here you may as well do something. \nFish? \nCatch bugs? \nHunt?");
+    string inputMountains = Console.ReadLine();
 
     string fish = "fish";
     string bugs = "bugs";
     string hunt = "hunt";
 
-    if (input3 == fish)
+    if (inputMountains == fish)
     {
-        Console.WriteLine($"Testing you might and patience you settled near a pond and cast your line... this is gonna take a while.");
+        Console.WriteLine($"Testing you might and patience you settled near a pond and cast your line to {fish}... this is gonna take a while.");
     }
 
-    else if (input3 == bugs)
+    else if (inputMountains == bugs)
     {
-        Console.WriteLine($"Hope you know those creatures better than I do, but sure go grab them with your bare hands. \nHope nothing poisonus is around...");
+        Console.WriteLine($"Hope you know those creatures better than I do, but sure go grab those {bugs} with your bare hands. \nHope nothing poisonus is around...");
     }
 
-    else if (input3 == hunt)
+    else if (inputMountains == hunt)
     {
-        Console.WriteLine($"Thankfully you remebered your gear. Congrats you were able to hunt");
+        Console.WriteLine($"Thankfully you remebered your gear. Congrats you were able to {hunt}");
     }
 
 }
 
 else if (inputLocation == town)
 {
-    Console.WriteLine($"You decided to town for whatever you might need off the top of your head.");
-    string input4 = Console.ReadLine();
+    Console.WriteLine($"You decided to {town} for whatever you might need off the top of your head. While you're there, what would you like to do? \nVisit a cafe? \n Go to a movie? \nGo shopping?");
+    string inputTown = Console.ReadLine();
 
     string cafe = "cafe";
     string movie = "movie";
     string shopping = "shopping";
 
-    if (input4 == cafe)
+    if (inputTown == cafe)
     {
-        Console.WriteLine($"You decided to settle down in a the quiet nook of a cafe, time to enjoy some coffee and sweets. If only you had a book to accompany it all.");
+        Console.WriteLine($"You decided to settle down in a the quiet nook of a {cafe}, time to enjoy some coffee and sweets. If only you had a book to accompany it all.");
     }
     // maybe I should make it an r rated movie to try and use a bool to check if the person is of age
-    else if (input4 == movie)
+    // now I gotta figure out how
+    else if (inputTown == movie)
     {
-        Console.WriteLine($"You bought a ticket to the afternoon show of a new blockbuster hit but its R18. Are you over 18?");
+        Console.WriteLine($"You go to buy a ticket to the afternoon show of a new blockbuster {movie}, grab some snacks and enjoy!");
     }
 
-    else if (input4 == shopping)
+    else if (inputTown == shopping)
     {
-        Console.WriteLine($"You went on a shopping spree for all sorts of clothes, accessories and knick-knacks. Hope you like them.");
+        Console.WriteLine($"You went on a {shopping} spree for all sorts of clothes, accessories and knick-knacks. Hope you like them.");
     }
+
 
 }
-
 else
 {
     Console.WriteLine($"You're not gonna shirk a free vacation, are you?");
 }
-
